51da843fafd5ac98a62d00d7be4d8d4c9376debb
DaniJS <159505889+Dexsless@users.noreply.github.com>
2026-01-21 01:03:09 +0700

    v0.0.4
    fix bug migrate setting

--- database/settings/0000_00_00_000034_create_task_settings.php
+++ database/settings/0000_00_00_000034_create_task_settings.php
@@ -12,26 +12,28 @@ return new class extends SettingsMigration
         $this->migrator->add('task.is_project_required', 'yes');
         $this->migrator->add('task.default_task_status', 6);
         $this->migrator->add('task.taskboard_length', 20);
-        $this->migrator->add('task.visible_to_client.task_category', true);
-        $this->migrator->add('task.visible_to_client.project', true);
-        $this->migrator->add('task.visible_to_client.start_date', true);
-        $this->migrator->add('task.visible_to_client.due_date', true);
-        $this->migrator->add('task.visible_to_client.assigned_to', true);
-        $this->migrator->add('task.visible_to_client.description', true);
-        $this->migrator->add('task.visible_to_client.label', true);
-        $this->migrator->add('task.visible_to_client.assigned_by', true);
-        $this->migrator->add('task.visible_to_client.status', true);
-        $this->migrator->add('task.visible_to_client.priority', true);
-        $this->migrator->add('task.visible_to_client.make_private', true);
-        $this->migrator->add('task.visible_to_client.time_estimate', true);
-        $this->migrator->add('task.visible_to_client.comments', true);
-        $this->migrator->add('task.visible_to_client.files_tab', true);
-        $this->migrator->add('task.visible_to_client.sub_task', true);
-        $this->migrator->add('task.visible_to_client.time_logs', true);
-        $this->migrator->add('task.visible_to_client.notes', true);
-        $this->migrator->add('task.visible_to_client.history', true);
-        $this->migrator->add('task.visible_to_client.hours_logged', true);
-        $this->migrator->add('task.visible_to_client.custom_fields', true);
-        $this->migrator->add('task.visible_to_client.copy_task_link', true);
+        $this->migrator->add('task.visible_to_client', [
+            'task_category' => true,
+            'project' => true,
+            'start_date' => true,
+            'due_date' => true,
+            'assigned_to' => true,
+            'description' => true,
+            'label' => true,
+            'assigned_by' => true,
+            'status' => true,
+            'priority' => true,
+            'make_private' => true,
+            'time_estimate' => true,
+            'comments' => true,
+            'files_tab' => true,
+            'sub_task' => true,
+            'time_logs' => true,
+            'notes' => true,
+            'history' => true,
+            'hours_logged' => true,
+            'custom_fields' => true,
+            'copy_task_link' => true,
+        ]);
     }
 };
--- database/settings/0000_00_00_000036_create_ticket_settings.php
+++ database/settings/0000_00_00_000036_create_ticket_settings.php
@@ -7,78 +7,45 @@ return new class extends SettingsMigration
     public function up(): void
     {
         $this->migrator->add('ticket.agents', []);
+
         $this->migrator->add('ticket.groups', [
             [
                 'id' => 1,
                 'name' => 'Support',
             ],
         ]);
+
         $this->migrator->add('ticket.types', [
-            [
-                'id' => 10,
-                'name' => 'Bug',
-            ],
-            [
-                'id' => 11,
-                'name' => 'Suggestion',
-            ],
-            [
-                'id' => 12,
-                'name' => 'Question',
-            ],
-            [
-                'id' => 13,
-                'name' => 'Sales',
-            ],
-            [
-                'id' => 14,
-                'name' => 'Code',
-            ],
-            [
-                'id' => 15,
-                'name' => 'Management',
-            ],
-            [
-                'id' => 16,
-                'name' => 'Problem',
-            ],
-            [
-                'id' => 17,
-                'name' => 'Incident',
-            ],
-            [
-                'id' => 18,
-                'name' => 'Feature Request',
-            ],
+            ['id' => 10, 'name' => 'Bug'],
+            ['id' => 11, 'name' => 'Suggestion'],
+            ['id' => 12, 'name' => 'Question'],
+            ['id' => 13, 'name' => 'Sales'],
+            ['id' => 14, 'name' => 'Code'],
+            ['id' => 15, 'name' => 'Management'],
+            ['id' => 16, 'name' => 'Problem'],
+            ['id' => 17, 'name' => 'Incident'],
+            ['id' => 18, 'name' => 'Feature Request'],
         ]);
+
         $this->migrator->add('ticket.channels', [
-            [
-                'id' => 5,
-                'name' => 'Email',
-            ],
-            [
-                'id' => 6,
-                'name' => 'Phone',
-            ],
-            [
-                'id' => 7,
-                'name' => 'Twitter',
-            ],
-            [
-                'id' => 8,
-                'name' => 'Facebook',
-            ],
+            ['id' => 5, 'name' => 'Email'],
+            ['id' => 6, 'name' => 'Phone'],
+            ['id' => 7, 'name' => 'Twitter'],
+            ['id' => 8, 'name' => 'Facebook'],
         ]);
+
         $this->migrator->add('ticket.reply_templates', []);
+
         $this->migrator->add('ticket.round_robin', [
             'enabled' => false,
             'method'  => 'sequential',
         ]);
+
         $this->migrator->add('ticket.visibility', [
             'client_can_view' => true,
             'client_can_reply' => true,
+            'scope' => 'assigned_tickets',
+            'group_ids' => [1],
         ]);
-        $this->migrator->add('ticket.visibility.scope', 'assigned_tickets');
-        $this->migrator->add('ticket.visibility.group_ids', [1]);
     }
 };
